24c59729e598154ffb928cd16a67e6fe1afe3035
Heather Jackson <hmjackson917@gmail.com>
2021-06-02 18:55:54 -0400

    bug fixes for product and tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,10 +8,11 @@ router.get('/', async (req, res) => {
   // find all products
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag }],
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: "product_tags" }],
     });
     res.status(200).json(productData);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
@@ -21,7 +22,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag }],
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: "product_tags" }],
     });
 
     if (!productData) {
